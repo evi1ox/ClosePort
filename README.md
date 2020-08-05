ad742efc44711253cc61bad70e85d6714e12397d
evi1ox <dropboy@qq.com>
2020-08-05 18:00:45 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# ClosePort
+
+一个关闭 135, 139, 445 的程序, 运行请自行检查 md5
+
+主要公司有大量主机需要关闭, 又不想要批处理下发, 所以有了这个产物
+
+```
+> md5 closeport_net*
+MD5 (closeport_net3.5.exe) = e751fd121e490bb76f57cb1e3de988bb
+MD5 (closeport_net4.0.exe) = 5b6983211c7c428fdc551c78f9dd5511
+```
+
+
+
+### 备注
+
+需要重启.就这样
---
+++ closeport/Program.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Linq;
+using System.ServiceProcess;
+
+namespace service
+{
+    class Program
+    {
+
+        static public bool DisableSc(string ScName)
+        {
+            try
+            {
+                RegistryKey NetBT = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Services\" + ScName, true);
+                NetBT.SetValue("Start", 4, RegistryValueKind.DWord);
+                NetBT.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error: {0}", err);
+                return false;
+            }
+
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            var serviceControllers = ServiceController.GetServices();
+            string[] ScName = { "MSDTC", "lmhosts", "SessionEnv", "Browser", "LanmanServer", "LanmanWorkstation" };
+            foreach (var ServiceName in ScName)
+            {
+                var server = serviceControllers.FirstOrDefault(service => service.ServiceName == ServiceName);
+                if (server != null)
+                {
+                    if (server.Status == ServiceControllerStatus.Running)
+                    {
+                        server.Stop();
+                    }
+
+                    if (ServiceName == "SessionEnv")
+                    {
+                        continue;
+                    }
+                    DisableSc(ServiceName);
+                }
+            }
+
+            //禁用135
+            try
+            {
+                RegistryKey NetBT = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Ole", true);
+                NetBT.SetValue("EnableDCOM", "N", RegistryValueKind.String);
+                NetBT.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error: {0}", err);
+            }
+
+            try
+            {
+                RegistryKey NetBT = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Rpc", true);
+                NetBT.SetValue("DCOM Protocols", new string[] { }, RegistryValueKind.MultiString);
+                NetBT.CreateSubKey("Internet");
+                NetBT.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error: {0}", err);
+            }
+
+
+            //禁用NetBIOS 139
+            try
+            {
+                RegistryKey NetBT = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Services\NetBT", true);
+                NetBT.SetValue("Start", 4, RegistryValueKind.DWord);
+                NetBT.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error: {0}", err);
+            }
+
+            // 登录时无须按 Ctrl+Alt+Del
+            try
+            {
+                RegistryKey NetBT = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\Currentversion\Policies\System", true);
+                NetBT.SetValue("disablecad", 0, RegistryValueKind.DWord);
+                NetBT.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error: {0}", err);
+            }
+
+
+            // 关闭445端口
+            try
+            {
+                RegistryKey NetBT = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\services\NetBT\Parameters", true);
+                NetBT.SetValue("SMBDeviceEnabled", 0, RegistryValueKind.DWord);
+                NetBT.Close();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error: {0}", err);
+            }
+
+            Console.WriteLine("Success, Please restart the computer to completely close the port! ");
+
+        }
+    }
+}
